59d12954b79c57b30399f727b57e56be39314d95
f-marinelli <fabri.marinelli@gmail.com>
2022-04-11 10:27:17 +0200

    n6

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import Bargraphs from './sub-forms/Bargraphs';
 import { generateHtml } from '../helper/generateHtml';
 import { generateCss } from '../helper/generateCss';
 import { sendData } from '../helper/sendData';
-import fetch from '/Users/fabri/Documents/Fabrizio/front-diag-serverless/api/fetch.js';
+import fetch from '../../api/fetch';
 
 const Form = () => {
   const typeDiag = useRef();
